c6fb6699e93af239b5ce94d9d275de42701e6be1
Aros24 <106389012+Aros24@users.noreply.github.com>
2025-02-23 17:03:05 +0100

    init java project commit

---
+++ swiftcode-decoder-api/src/main/java/com/exercise/swiftcode/swiftcode_decoder_api/SwiftCodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.swiftcode.swiftcode_decoder_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCodeApiApplication.class, args);
+	}
+
+}
